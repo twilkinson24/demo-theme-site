85e129102ad224a1d98d7f9698b5796c29ef379c
Taylor <taylorwteaches@gmail.com>
2021-03-06 11:29:58 -0500

    using template parts for homepage sections

--- loop-templates/content-homepage.php
+++ loop-templates/content-homepage.php
@@ -18,55 +18,13 @@ if (!class_exists('ACF')) {
     echo '<p>Please (a) install and/or activate the ACF plugin or (B) use a different page template.'; 
 } else {
 
-    // ACF variables
+    /*
+    * Homepage sections
+    */
+    get_template_part('template-parts/home-section-one'); 
 
-    /* About Section */
-    $perch_home_about_section_content = get_field('perch_home_about_section_content');
-    // slider text content
-    $perch_home_about_show_slider = get_field('perch_home_about_show_slider');
-    $perch_home_about_slide_content_1 = get_field('perch_home_about_slide_content_1');
-    $perch_home_about_slide_content_2 = get_field('perch_home_about_slide_content_2');
-    $perch_home_about_slide_content_3 = get_field('perch_home_about_slide_content_3');
-    $perch_home_about_slide_content_4 = get_field('perch_home_about_slide_content_4');
-    // slide images
-    $perch_home_about_slide_img_1 = get_field('perch_home_about_slide_img_1');
-    $perch_home_about_slide_img_2 = get_field('perch_home_about_slide_img_2');
-    $perch_home_about_slide_img_3 = get_field('perch_home_about_slide_img_3');
-    $perch_home_about_slide_img_4 = get_field('perch_home_about_slide_img_4');
+    
 }
 
 
 ?>
-
-<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-
-	<header class="entry-header">
-
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-
-	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
-
-		<?php the_content(); ?>
-
-		<?php
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
-
-	</footer><!-- .entry-footer -->
-
-</article><!-- #post-## -->
--- page-templates/homepage.php
+++ page-templates/homepage.php
@@ -38,13 +38,6 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 						<?php get_template_part( 'loop-templates/content', 'homepage' ); ?>
 
-						<?php
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-						?>
-
 					<?php endwhile; // end of the loop. ?>
 
 				</main><!-- #main -->
